9ff723bc927fa809be677184bdae73fee20dbc9d
lanomade <ganne.hannah@gmail.com>
2022-10-05 14:17:01 +0200

    extract some code into components

--- argent-bank/src/pages/Signin.jsx
+++ argent-bank/src/pages/Signin.jsx
@@ -2,6 +2,10 @@ import Main from '../components/Main'
 import Section from '../components/Section'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import Input from '../components/Input'
+import InputTitle from '../components/InputTitle'
+import InputField from '../components/InputField'
+import Button from '../components/Button'
 
 export default function Signin() {
     return (
@@ -10,19 +14,19 @@ export default function Signin() {
                 <FontAwesomeIcon icon={faUserCircle} className="sign-in-icon"/>
                 <h1>Sign In</h1>
                 <form>
-                    <div class="input-wrapper">
-                        <label for="username">Username</label>
-                        <input type="text" id="username" />
-                    </div>
-                    <div class="input-wrapper">
-                        <label for="password">Password</label>
-                        <input type="password" id="password" />
-                    </div>
-                    <div class="input-remember">
-                        <input type="checkbox" id="remember-me" />
-                        <label for="remember-me">Remember me</label>
-                    </div>
-                    <button class="sign-in-button">Sign In</button>
+                    <Input className="input-wrapper">
+                        <InputTitle id="username" label="Username" />
+                        <InputField id="username" type="text" />
+                    </Input>
+                    <Input className="input-wrapper">
+                        <InputTitle id="password" label="Password" />
+                        <InputField id="password" type="password" />
+                    </Input>
+                    <Input className="input-remember">
+                        <InputField id="remember-me" type="checkbox" />
+                        <InputTitle id="remember-me" label="Remember me" />
+                    </Input>
+                    <Button className="sign-in-button" label="Sign In" />
                 </form>
             </Section>   
         </Main>
