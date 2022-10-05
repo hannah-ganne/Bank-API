5282272ad47761ec78f4e53c68496ed810201203
lanomade <ganne.hannah@gmail.com>
2022-10-05 14:17:09 +0200

    create Transactions component

---
+++ argent-bank/src/pages/Transactions.jsx
@@ -0,0 +1,13 @@
+import Main from '../components/Main'
+
+export default function Transactions() {
+    return (
+        <Main className="main bg-gray">
+            <div className="header ft-dark">
+                <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+                <p className="account-amount">$2,082.79</p>
+                <p className="account-amount-description">Available balance</p>
+            </div>
+        </Main>
+    )
+}
