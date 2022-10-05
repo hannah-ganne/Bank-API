2a6fc0f966afae45aee2211e74858d5febb83b19
lanomade <ganne.hannah@gmail.com>
2022-10-05 16:05:55 +0200

    create Transactions component

--- argent-bank/src/pages/Transactions.jsx
+++ argent-bank/src/pages/Transactions.jsx
@@ -1,4 +1,11 @@
 import Main from '../components/Main'
+import Accordion from '../components/Accordion'
+import AccordionSection from '../components/AccordionSection'
+import AccordionTitle from '../components/AccordionTitle'
+import AccordionContent from '../components/AccordionContent'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import GridTitle from '../components/GridTitle'
 
 export default function Transactions() {
     return (
@@ -8,6 +15,64 @@ export default function Transactions() {
                 <p className="account-amount">$2,082.79</p>
                 <p className="account-amount-description">Available balance</p>
             </div>
+            <Accordion>
+                <AccordionSection>
+                    <AccordionTitle>
+                        <FontAwesomeIcon icon={faAngleDown} size="lg" />
+                        <GridTitle />
+                    </AccordionTitle>
+                    <AccordionContent>
+                        <p>Transaction Type: Electronic</p>
+                        <p>Category: Food</p>
+                        <p>Notes:</p>
+                    </AccordionContent>
+                </AccordionSection>
+                <AccordionSection>
+                    <AccordionTitle>
+                        <FontAwesomeIcon icon={faAngleDown} size="lg" />
+                        <GridTitle />
+                    </AccordionTitle>
+                    <AccordionContent>
+                        Transaction 1
+                    </AccordionContent>
+                </AccordionSection>
+                <AccordionSection>
+                    <AccordionTitle>
+                        <FontAwesomeIcon icon={faAngleDown} size="lg" />
+                        <GridTitle />
+                    </AccordionTitle>
+                    <AccordionContent>
+                        Transaction 1
+                    </AccordionContent>
+                </AccordionSection>
+                <AccordionSection>
+                    <AccordionTitle>
+                        <FontAwesomeIcon icon={faAngleDown} size="lg" />
+                        <GridTitle />
+                    </AccordionTitle>
+                    <AccordionContent>
+                        Transaction 1
+                    </AccordionContent>
+                </AccordionSection>
+                <AccordionSection>
+                    <AccordionTitle>
+                        <FontAwesomeIcon icon={faAngleDown} size="lg" />
+                        <GridTitle />
+                    </AccordionTitle>
+                    <AccordionContent>
+                        Transaction 1
+                    </AccordionContent>
+                </AccordionSection>
+                <AccordionSection>
+                    <AccordionTitle>
+                        <FontAwesomeIcon icon={faAngleDown} size="lg" />
+                        <GridTitle />
+                    </AccordionTitle>
+                    <AccordionContent>
+                        Transaction 1
+                    </AccordionContent>
+                </AccordionSection>
+            </Accordion>
         </Main>
     )
 }
