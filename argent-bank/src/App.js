eaf0fba8fa969e5949cfaed9f52dfd06c2c99f3c
lanomade <ganne.hannah@gmail.com>
2022-10-05 12:31:39 +0200

    add Outlet component from react router

--- argent-bank/src/App.js
+++ argent-bank/src/App.js
@@ -1,31 +1,13 @@
-import './App.css';
 import Header from './components/Header'
-import Main from './components/Main'
-import Hero from './components/Hero'
-import Section from './components/Section'
-import { features } from './assets/data/features'
-import Feature from './components/Feature'
 import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
     return (
         <>
             <Header />
-            <Main>
-                <Hero />
-                <Section className="features">
-                    <h2 class="sr-only">Features</h2>
-                    {features.map(item =>
-                        <Feature
-                            img={item.img}
-                            alt={item.alt}
-                            title={item.title}
-                            description={item.description} 
-                        />
-                    )}
-                </Section>
-            </Main>
+            <Outlet />
             <Footer />
         </>
     )
