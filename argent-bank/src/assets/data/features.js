c7724f3107a349092a6c669b6867939530e06c43
lanomade <ganne.hannah@gmail.com>
2022-10-05 11:34:20 +0200

    create data array for features

---
+++ argent-bank/src/assets/data/features.js
@@ -0,0 +1,24 @@
+import chat from '../images/icon-chat.png'
+import money from '../images/icon-money.png'
+import security from '../images/icon-security.png'
+
+export const features = [
+    {
+        img: chat,
+        alt: "Chat Icon",
+        title: "You are our #1 priority",
+        description: "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+    },
+    {
+        img: money,
+        alt: "Money Icon",
+        title: "More savings means higher rates",
+        description: "The more you save with us, the higher your interest rate will be!"
+    },
+    {
+        img: security,
+        alt: "Security Icon",
+        title: "Security you can trust",
+        description: "We use top of the line encryption to make sure your data and money is always safe."
+    }
+]
