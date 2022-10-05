9bdba5a760e5fb9f3c9d1a8212d1449b46cab2e2
lanomade <ganne.hannah@gmail.com>
2022-10-05 16:05:47 +0200

    create Grid Title component

---
+++ argent-bank/src/components/GridTitle.jsx
@@ -0,0 +1,10 @@
+export default function GridTitle() {
+    return (
+        <div data-panel-title-grid>
+            <p>June 20th, 2020</p>
+            <p>Golden Sun Bakery</p>
+            <p>$5.00</p>
+            <p>$2082.79</p>
+        </div>
+    )
+}
