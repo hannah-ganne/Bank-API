3e290b2bda4ed1058baa929bc9fb76cac2577789
lanomade <ganne.hannah@gmail.com>
2022-10-06 20:28:16 +0200

    add Provider

--- argent-bank/src/index.js
+++ argent-bank/src/index.js
@@ -7,19 +7,23 @@ import Home from './pages/Home'
 import Signin from './pages/Signin'
 import User from './pages/User'
 import Transactions from './pages/Transactions'
+import { store } from './utils/redux/store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} >
-          <Route path="/" element={<Home />} />
-          <Route path="signin" element={<Signin />} />
-          <Route path="user" element={<User />} />
-          <Route path="transactions" element={<Transactions />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} >
+            <Route path="/" element={<Home />} />
+            <Route path="signin" element={<Signin />} />
+            <Route path="user" element={<User />} />
+            <Route path="transactions" element={<Transactions />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
