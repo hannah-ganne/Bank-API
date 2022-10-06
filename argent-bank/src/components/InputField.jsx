8f24e44c69bcd1a31639f1cb525f35544e39dac9
lanomade <ganne.hannah@gmail.com>
2022-10-06 20:28:34 +0200

    turn InputField controlled component

--- argent-bank/src/components/InputField.jsx
+++ argent-bank/src/components/InputField.jsx
@@ -1,3 +1,7 @@
-export default function InputField({ id, type }) {
-    return <input type={type} id={id} />
+import { useState } from 'react'
+
+export default function InputField({ id, type, size = 20, onSubmit }) {
+    const [value, setValue] = useState('')
+
+    return <input type={type} id={id} size={size} onChange={event => setValue(event.target.value)} value={value} />
 }
