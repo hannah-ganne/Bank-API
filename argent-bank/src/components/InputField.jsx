663732350e78d896d96868a972f5425b0b987988
lanomade <ganne.hannah@gmail.com>
2022-10-06 22:55:52 +0200

    add onKeyPress event handler

--- argent-bank/src/components/InputField.jsx
+++ argent-bank/src/components/InputField.jsx
@@ -1,7 +1,15 @@
 import { useState } from 'react'
 
-export default function InputField({ id, type, size = 20, onSubmit }) {
-    const [value, setValue] = useState('')
+export default function InputField({id, type, size = 20, value, setValue, onKeyPress }) {
 
-    return <input type={type} id={id} size={size} onChange={event => setValue(event.target.value)} value={value} />
+    return (
+        <input
+            type={type}
+            id={id}
+            size={size}
+            onChange={event => setValue(event.target.value)}
+            value={value}
+            onKeyPress={onKeyPress} 
+        />
+    )
 }
