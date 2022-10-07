0d7d30d6bf03db043d91e3cb7c59ac902bd3b387
lanomade <ganne.hannah@gmail.com>
2022-10-07 11:19:50 +0200

    add signin logic

--- argent-bank/src/pages/Signin.jsx
+++ argent-bank/src/pages/Signin.jsx
@@ -6,21 +6,40 @@ import Input from '../components/Input'
 import InputTitle from '../components/InputTitle'
 import InputField from '../components/InputField'
 import Button from '../components/Button'
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectUser, login } from '../utils/redux/userSlice'
+import { Navigate } from 'react-router-dom'
 
 export default function Signin() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const user = useSelector(selectUser)
+    const dispatch = useDispatch()
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        dispatch(login({ email: username, password: password }))
+    }
+
     return (
         <Main className="main bg-dark">
             <Section className="sign-in-content">
                 <FontAwesomeIcon icon={faUserCircle} className="sign-in-icon"/>
                 <h1>Sign In</h1>
-                <form>
+                {user.hasError && <p>There is an error</p>}
+                {(user.token && !user.isLoading && !user.hasError) && (
+                    <Navigate to="/user" replace={true} />
+                )}
+                <form onSubmit={handleSubmit}>
                     <Input className="input-wrapper">
                         <InputTitle id="username" label="Username" />
-                        <InputField id="username" type="text" />
+                        <InputField id="username" type="text" value={username} setValue={setUsername} />
                     </Input>
                     <Input className="input-wrapper">
                         <InputTitle id="password" label="Password" />
-                        <InputField id="password" type="password" />
+                        <InputField id="password" type="password" value={password} setValue={setPassword} />
                     </Input>
                     <Input className="input-remember">
                         <InputField id="remember-me" type="checkbox" />
