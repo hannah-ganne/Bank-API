744fd69ab981a35be112f4524774068391df97c4
lanomade <ganne.hannah@gmail.com>
2022-10-07 14:45:05 +0200

    add useNavigate for user page

--- argent-bank/src/components/Account.jsx
+++ argent-bank/src/components/Account.jsx
@@ -1,15 +1,21 @@
 import Button from './Button'
+import { useNavigate } from 'react-router-dom'
 
-export default function Account({ title, amount, description }) {
+export default function Account({ index, title, amount, description }) {
+    const navigate = useNavigate()
+    const viewTransaction = () => {
+        navigate(`/transactions/${index}`)
+    }
+
     return (
     <>
         <div className="account-content-wrapper">
             <h3 className="account-title">{title}</h3>
-            <p className="account-amount">{`$${amount}`}</p>
+            <p className="account-amount">{`$${amount.toFixed(2)}`}</p>
             <p className="account-amount-description">{description}</p>
         </div>
         <div className="account-content-wrapper cta">
-            <Button className="transaction-button" label="View Transactions" />
+            <Button className="transaction-button" label="View Transactions" onClick={viewTransaction} />
         </div>
     </>    
     )
