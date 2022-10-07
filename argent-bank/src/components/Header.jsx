75b573cf5fdc0fc2238ccc950f728d2c244dbe78
lanomade <ganne.hannah@gmail.com>
2022-10-07 11:19:02 +0200

    add sing out logic

--- argent-bank/src/components/Header.jsx
+++ argent-bank/src/components/Header.jsx
@@ -2,8 +2,19 @@ import logo from '../assets/images/argentBankLogo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectUser, logout } from '../utils/redux/userSlice'
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
+    const user = useSelector(selectUser)
+    const dispatch = useDispatch()
+
+    const handleSignOut = () => {
+        dispatch(logout())
+        console.log(user)
+    }
+
     return (
         <nav className="main-nav">
             <Link to="/" className="main-nav-logo">
@@ -15,10 +26,27 @@ export default function Header() {
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <div>
-                <Link to="/signin" className="main-nav-item">
-                    <FontAwesomeIcon icon={faUserCircle} />
-                    Sign In
-                </Link>
+                {
+                    user.token ?
+                    (
+                        <div className="header-user">       
+                            <Link to="/user" className="main-nav-item">
+                            <FontAwesomeIcon icon={faUserCircle} />
+                            {user.firstName}    
+                            </Link>
+                            <Link className="main-nav-item" onClick={handleSignOut}>
+                                <FontAwesomeIcon icon={faRightFromBracket} />
+                                Sign out
+                            </Link>
+                        </div>
+                    ) :
+                    (
+                        <Link to="/signin" className="main-nav-item">
+                            <FontAwesomeIcon icon={faUserCircle} />
+                            Sign In
+                        </Link>
+                    )  
+                }
             </div>
         </nav>
     )
